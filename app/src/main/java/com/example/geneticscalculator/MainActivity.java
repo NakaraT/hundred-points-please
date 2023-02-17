9c6e0bdd6da15da3def8fe99db5589ac55312a1b
NakaraT <slava.tarakanov@mail.ru>
2023-02-17 22:21:37 +0300

    Added logs

--- app/src/main/java/com/example/geneticscalculator/MainActivity.java
+++ app/src/main/java/com/example/geneticscalculator/MainActivity.java
@@ -3,16 +3,18 @@ package com.example.geneticscalculator;
 import androidx.appcompat.app.AppCompatActivity;
 
 import android.os.Bundle;
+import android.util.Log;
 import android.view.Gravity;
 import android.widget.Toast;
 
 public class MainActivity extends AppCompatActivity {
-
+    private static final String TAG = "geneticscalculator";
     protected void onCreate(Bundle savedInstanceState) {
         super.onCreate(savedInstanceState);
         Toast toast1 = Toast.makeText(getApplicationContext(),
                 "Приложение создано.", Toast.LENGTH_SHORT);
         toast1.show();
+        Log.i(TAG, "Приложение было успешно создано");
         setContentView(R.layout.activity_main);
     }
 
@@ -21,7 +23,7 @@ public class MainActivity extends AppCompatActivity {
         Toast toast2 = Toast.makeText(getApplicationContext(),
                 "Приложение закрыто.", Toast.LENGTH_SHORT);
         toast2.show();
-
+        Log.e(TAG, "Приложение было успешно закрыто");
     }
 
     protected void onRestart() {
@@ -29,6 +31,7 @@ public class MainActivity extends AppCompatActivity {
         Toast toast3 = Toast.makeText(getApplicationContext(),
                 "Приложение перезапущено.", Toast.LENGTH_SHORT);
         toast3.show();
+        Log.v(TAG, "Приложение было успешно перезапущено");
     }
 
     protected void onPause() {
@@ -36,6 +39,7 @@ public class MainActivity extends AppCompatActivity {
         Toast toast4 = Toast.makeText(getApplicationContext(),
                 "Приложение было приостановлено.", Toast.LENGTH_SHORT);
         toast4.show();
+        Log.w(TAG, "Приложение было успешно приостановлено");
     }
 
     protected void onStop() {
@@ -43,6 +47,7 @@ public class MainActivity extends AppCompatActivity {
         Toast toast5 = Toast.makeText(getApplicationContext(),
                 "Приложение было остановлено.", Toast.LENGTH_SHORT);
         toast5.show();
+        Log.d(TAG, "Приложение было успешно остановлено");
     }
 
     protected void onStart() {
@@ -50,6 +55,7 @@ public class MainActivity extends AppCompatActivity {
         Toast toast6 = Toast.makeText(getApplicationContext(),
                 "Приложение было запущено.", Toast.LENGTH_SHORT);
         toast6.show();
+        Log.i(TAG, "Приложение было успешно запущено");
     }
 
     protected void onResume() {
@@ -57,5 +63,6 @@ public class MainActivity extends AppCompatActivity {
         Toast toast7 = Toast.makeText(getApplicationContext(),
                 "Приложение было возобновлено.", Toast.LENGTH_SHORT);
         toast7.show();
+        Log.v(TAG, "Приложение было успешно возобновлено");
     }
 }
