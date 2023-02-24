e7bbaada1e89bc1ddf9e6348a77bf076f74445fe
NakaraT <slava.tarakanov@mail.ru>
2023-02-24 19:16:38 +0300

    Adding an abstract onClick method

--- app/src/main/java/com/example/geneticscalculator/MainActivity.java
+++ app/src/main/java/com/example/geneticscalculator/MainActivity.java
@@ -5,64 +5,19 @@ import androidx.appcompat.app.AppCompatActivity;
 import android.os.Bundle;
 import android.util.Log;
 import android.view.Gravity;
+import android.view.View;
 import android.widget.Toast;
 
 public class MainActivity extends AppCompatActivity {
     private static final String TAG = "geneticscalculator";
     protected void onCreate(Bundle savedInstanceState) {
         super.onCreate(savedInstanceState);
-        Toast toast1 = Toast.makeText(getApplicationContext(),
-                "Приложение создано.", Toast.LENGTH_SHORT);
-        toast1.show();
-        Log.i(TAG, "Приложение было успешно создано");
         setContentView(R.layout.activity_main);
-    }
+        findViewById(R.id.button2).setOnClickListener(new View.OnClickListener() {
+            public void onClick(View view) {
+                Log.i(TAG, "Вывод log по нажатию!");
 
-    protected void onDestroy() {
-        super.onDestroy();
-        Toast toast2 = Toast.makeText(getApplicationContext(),
-                "Приложение закрыто.", Toast.LENGTH_SHORT);
-        toast2.show();
-        Log.e(TAG, "Приложение было успешно закрыто");
-    }
-
-    protected void onRestart() {
-        super.onRestart();
-        Toast toast3 = Toast.makeText(getApplicationContext(),
-                "Приложение перезапущено.", Toast.LENGTH_SHORT);
-        toast3.show();
-        Log.v(TAG, "Приложение было успешно перезапущено");
-    }
-
-    protected void onPause() {
-        super.onPause();
-        Toast toast4 = Toast.makeText(getApplicationContext(),
-                "Приложение было приостановлено.", Toast.LENGTH_SHORT);
-        toast4.show();
-        Log.w(TAG, "Приложение было успешно приостановлено");
-    }
-
-    protected void onStop() {
-        super.onStop();
-        Toast toast5 = Toast.makeText(getApplicationContext(),
-                "Приложение было остановлено.", Toast.LENGTH_SHORT);
-        toast5.show();
-        Log.d(TAG, "Приложение было успешно остановлено");
-    }
-
-    protected void onStart() {
-        super.onStart();
-        Toast toast6 = Toast.makeText(getApplicationContext(),
-                "Приложение было запущено.", Toast.LENGTH_SHORT);
-        toast6.show();
-        Log.i(TAG, "Приложение было успешно запущено");
-    }
-
-    protected void onResume() {
-        super.onResume();
-        Toast toast7 = Toast.makeText(getApplicationContext(),
-                "Приложение было возобновлено.", Toast.LENGTH_SHORT);
-        toast7.show();
-        Log.v(TAG, "Приложение было успешно возобновлено");
-    }
+            }
+        });
+    };
 }
