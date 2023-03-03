18ed46333b769f83a1342bd7b35929df480d1080
NakaraT <slava.tarakanov@mail.ru>
2023-03-03 09:52:26 +0300

    Creating logic for buttons,сreating a second activity

--- app/src/main/java/com/example/geneticscalculator/MainActivity.java
+++ app/src/main/java/com/example/geneticscalculator/MainActivity.java
@@ -4,20 +4,25 @@ import androidx.appcompat.app.AppCompatActivity;
 
 import android.os.Bundle;
 import android.util.Log;
-import android.view.Gravity;
 import android.view.View;
-import android.widget.Toast;
+import android.content.Intent;
+import android.widget.Button;
 
-public class MainActivity extends AppCompatActivity {
+public class MainActivity extends AppCompatActivity implements View.OnClickListener{
     private static final String TAG = "geneticscalculator";
     protected void onCreate(Bundle savedInstanceState) {
         super.onCreate(savedInstanceState);
         setContentView(R.layout.activity_main);
-        findViewById(R.id.button2).setOnClickListener(new View.OnClickListener() {
-            public void onClick(View view) {
-                Log.i(TAG, "Вывод log по нажатию!");
+        Button buttonBack = (Button)findViewById(R.id.button1);
+        buttonBack.setOnClickListener(this);
+    }
 
-            }
-        });
-    };
+    public void onClick(View view) {
+        Intent intent = new Intent(this, SecondActivity.class);
+        startActivity(intent);
+    }
+
+    public void Click2(View view) {
+        Log.i(TAG, "Вывод log по нажатию!");
+    }
 }
