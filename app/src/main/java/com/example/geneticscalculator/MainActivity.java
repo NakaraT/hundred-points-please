c68ab894a2bd2d241c4f2af7ba3704ce91c03fb2
NakaraT <slava.tarakanov@mail.ru>
2023-03-03 10:25:51 +0300

    Implementation of data transfer between the Activity, creation of a button in the second activity with the ability to return to the main screen

--- app/src/main/java/com/example/geneticscalculator/MainActivity.java
+++ app/src/main/java/com/example/geneticscalculator/MainActivity.java
@@ -8,19 +8,28 @@ import android.view.View;
 import android.content.Intent;
 import android.widget.Button;
 
-public class MainActivity extends AppCompatActivity implements View.OnClickListener{
+public class MainActivity extends AppCompatActivity{
+
     private static final String TAG = "geneticscalculator";
+    public static final String KEY = "key";
     protected void onCreate(Bundle savedInstanceState) {
         super.onCreate(savedInstanceState);
         setContentView(R.layout.activity_main);
-        Button buttonBack = (Button)findViewById(R.id.button1);
-        buttonBack.setOnClickListener(this);
+        Button button1 = findViewById(R.id.button1);
+        button1.setOnClickListener(new View.OnClickListener() {
+            @Override
+            public void onClick(View view) {
+                Intent intent = new Intent(MainActivity.this, SecondActivity.class);
+                intent.putExtra(KEY, "genetics");
+                startActivity(intent);
+            }
+        });
     }
 
-    public void onClick(View view) {
-        Intent intent = new Intent(this, SecondActivity.class);
-        startActivity(intent);
-    }
+    //public void onClick(View view) {
+    //    Intent intent = new Intent(this, SecondActivity.class);
+    //    startActivity(intent);
+    //}
 
     public void Click2(View view) {
         Log.i(TAG, "Вывод log по нажатию!");
