c9a785b4bdd1c5f7beee325236eaf8c004544964
NakaraT <slava.tarakanov@mail.ru>
2023-03-10 22:43:27 +0300

    Adding a display of all lifecycle methods

--- app/src/main/java/com/example/geneticscalculator/HomeFragment.java
+++ app/src/main/java/com/example/geneticscalculator/HomeFragment.java
@@ -1,15 +1,19 @@
 package com.example.geneticscalculator;
 
+import android.content.Context;
 import android.os.Bundle;
+import android.util.Log;
 import android.view.LayoutInflater;
 import android.view.View;
 import android.view.ViewGroup;
+import android.widget.Toast;
 
 import androidx.annotation.NonNull;
 import androidx.annotation.Nullable;
 import androidx.fragment.app.Fragment;
 
 import com.example.geneticscalculator.databinding.FragmentHomeBinding;
+import com.example.geneticscalculator.databinding.FragmentMenuBinding;
 
 
 public class HomeFragment extends Fragment {
@@ -20,13 +24,133 @@ public class HomeFragment extends Fragment {
 
     private FragmentHomeBinding binding;
     @Override
+    public void onAttach(@NonNull Context context) {
+        Log.d(TAG, "onAttach");
+        Toast.makeText(getContext(), "onAttach", Toast.LENGTH_SHORT).show();
+        super.onAttach(context);
+    }
+
+    @Override
+    public void onCreate(@Nullable Bundle savedInstanceState) {
+        Log.d(TAG, "OnCreate");
+        Log.d(TAG, "View Lifecycle = null");
+        Log.d(TAG, "Fragment Lifecycle = " + getLifecycle().getCurrentState().toString());
+        Toast.makeText(getContext(), "onCreate", Toast.LENGTH_SHORT).show();
+        super.onCreate(savedInstanceState);
+    }
+
+    @Override
     public View onCreateView(@NonNull LayoutInflater inflater, @Nullable ViewGroup container, @Nullable Bundle savedInstanceState) {
-        binding = FragmentHomeBinding.inflate(inflater, container, false);
+        Log.d(TAG, "OnCreateView");
+        Log.d(TAG, "View Lifecycle = " + getViewLifecycleOwner().getLifecycle().getCurrentState().toString());
+        Log.d(TAG, "Fragment Lifecycle = " + getLifecycle().getCurrentState().toString());
+        Toast.makeText(getContext(), "onCreateView", Toast.LENGTH_SHORT).show();
+
+        binding = FragmentHomeBinding.inflate(getLayoutInflater());
         return binding.getRoot();
     }
 
     @Override
     public void onViewCreated(@NonNull View view, @Nullable Bundle savedInstanceState) {
+
+        Log.d(TAG, "OnViewCreated");
+        Log.d(TAG, "View Lifecycle = " + getViewLifecycleOwner().getLifecycle().getCurrentState().toString());
+        Log.d(TAG, "Fragment Lifecycle = " + getLifecycle().getCurrentState().toString());
+        Toast.makeText(getContext(), "onViewCreated", Toast.LENGTH_SHORT).show();
+
         super.onViewCreated(view, savedInstanceState);
+    }
+
+    @Override
+    public void onViewStateRestored(@Nullable Bundle savedInstanceState) {
+
+        Log.d(TAG, "OnViewStateRestored");
+        Log.d(TAG, "View Lifecycle = " + getViewLifecycleOwner().getLifecycle().getCurrentState().toString());
+        Log.d(TAG, "Fragment Lifecycle = " + getLifecycle().getCurrentState().toString());
+        Toast.makeText(getContext(), "onViewStateRestored", Toast.LENGTH_SHORT).show();
+
+        super.onViewStateRestored(savedInstanceState);
+    }
+
+    @Override
+    public void onStart() {
+        Log.d(TAG, "OnStart");
+        Log.d(TAG, "View Lifecycle = " + getViewLifecycleOwner().getLifecycle().getCurrentState().toString());
+        Log.d(TAG, "Fragment Lifecycle = " + getLifecycle().getCurrentState().toString());
+        Toast.makeText(getContext(), "onStart", Toast.LENGTH_SHORT).show();
+        super.onStart();
+    }
+
+    @Override
+    public void onResume() {
+
+        Log.d(TAG, "OnResume");
+        Log.d(TAG, "View Lifecycle = " + getViewLifecycleOwner().getLifecycle().getCurrentState().toString());
+        Log.d(TAG, "Fragment Lifecycle = " + getLifecycle().getCurrentState().toString());
+        Toast.makeText(getContext(), "onResume", Toast.LENGTH_SHORT).show();
+
+        super.onResume();
+    }
+
+    @Override
+    public void onPause() {
+
+        Log.d(TAG, "OnPause");
+        Log.d(TAG, "View Lifecycle = " + getViewLifecycleOwner().getLifecycle().getCurrentState().toString());
+        Log.d(TAG, "Fragment Lifecycle = " + getLifecycle().getCurrentState().toString());
+        Toast.makeText(getContext(), "onPause", Toast.LENGTH_SHORT).show();
+
+        super.onPause();
+    }
+
+    @Override
+    public void onStop() {
+
+        Log.d(TAG, "OnStop");
+        Log.d(TAG, "View Lifecycle = " + getViewLifecycleOwner().getLifecycle().getCurrentState().toString());
+        Log.d(TAG, "Fragment Lifecycle = " + getLifecycle().getCurrentState().toString());
+        Toast.makeText(getContext(), "onStop", Toast.LENGTH_SHORT).show();
+
+        super.onStop();
+    }
+
+    @Override
+    public void onSaveInstanceState(@NonNull Bundle outState) {
+
+        Log.d(TAG, "onSaveInstanceState");
+        Log.d(TAG, "View Lifecycle = " + getViewLifecycleOwner().getLifecycle().getCurrentState().toString());
+        Log.d(TAG, "Fragment Lifecycle = " + getLifecycle().getCurrentState().toString());
+        Toast.makeText(getContext(), "onSaveInstanceState", Toast.LENGTH_SHORT).show();
+
+        super.onSaveInstanceState(outState);
+    }
+
+    @Override
+    public void onDestroyView() {
+
+        Log.d(TAG, "OnDestroyView");
+        Log.d(TAG, "View Lifecycle = " + getViewLifecycleOwner().getLifecycle().getCurrentState().toString());
+        Log.d(TAG, "Fragment Lifecycle = " + getLifecycle().getCurrentState().toString());
+        Toast.makeText(getContext(), "onDestroyView", Toast.LENGTH_SHORT).show();
+
+        super.onDestroyView();
+    }
+
+    @Override
+    public void onDestroy() {
+        Log.d(TAG, "OnDestroy");
+        Log.d(TAG, "View Lifecycle = null");
+        Log.d(TAG, "Fragment Lifecycle = " + getLifecycle().getCurrentState().toString());
+        Toast.makeText(getContext(), "onDestroy", Toast.LENGTH_SHORT).show();
+
+        super.onDestroy();
+    }
+
+    @Override
+    public void onDetach() {
+        Log.d(TAG, "onDetach");
+        Toast.makeText(getContext(), "onDetach", Toast.LENGTH_SHORT).show();
+
+        super.onDetach();
     }
 }
--- app/src/main/java/com/example/geneticscalculator/MenuFragment.java
+++ app/src/main/java/com/example/geneticscalculator/MenuFragment.java
@@ -1,34 +1,17 @@
 package com.example.geneticscalculator;
 
-import android.os.Bundle;
-
-import androidx.annotation.NonNull;
-import androidx.annotation.Nullable;
 import androidx.fragment.app.Fragment;
 
-import android.view.LayoutInflater;
-import android.view.View;
-import android.view.ViewGroup;
 
 import com.example.geneticscalculator.databinding.FragmentMenuBinding;
 
 
 public class MenuFragment extends Fragment {
     private static final String TAG = "geneticscalculator";
+
     public MenuFragment() {
         super(R.layout.fragment_menu);
     }
-
     private FragmentMenuBinding binding;
 
-    @Override
-    public View onCreateView(@NonNull LayoutInflater inflater, @Nullable ViewGroup container, @Nullable Bundle savedInstanceState) {
-        binding = FragmentMenuBinding.inflate(inflater, container, false);
-        return binding.getRoot();
-    }
-
-    @Override
-    public void onViewCreated(@NonNull View view, @Nullable Bundle savedInstanceState) {
-        super.onViewCreated(view, savedInstanceState);
-    }
 }
